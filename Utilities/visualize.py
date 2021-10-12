7a56d493e91647116be8b291c4b524cd82623092
yoosunyoung <luysunyoung9@gmail.com>
2021-10-12 13:33:32 +0900

    save sample cropped images

--- Utilities/visualize.py
+++ Utilities/visualize.py
@@ -1,30 +1,59 @@
 import numpy as np
 import pandas as pd
 import cv2
+import os
 
 
-def draw_rectangles(image_path, label_path, predicted_labels=None):
+def draw_raw_image(image_path, label_path, predicted_labels=None):
     image = np.load(image_path)
     labels = np.load(label_path)
     image_shape = image.shape
-    image = np.resize(image, (image_shape[0]*image_shape[1], 3))
-    image = np.array(list(map(lambda x: [int(x[2])], image)))
-    image = np.resize(image, (image_shape[0], image_shape[1], 1))
+    # image = np.resize(image, (image_shape[0]*image_shape[1], 3))
+    # image = np.array(list(map(lambda x: [int(x[2]), int(x[2]), int(x[2])], image)))
+    # image = np.resize(image, (image_shape[0], image_shape[1], 1))
     for label in labels:
-        x1y1 = (label[0]-label[2]/2, label[1]-label[2]/2)
-        x2y2 = (label[0]+label[2]/2, label[1]+label[2]/2)
-        image = cv2.rectangle(image, x1y1, x2y2,
-                              (255, 0, 0), 2)
+        x1y1 = (int(label[0]-label[2]/2), int(label[1]-label[2]/2))
+        x2y2 = (int(label[0]+label[2]/2), int(label[1]+label[2]/2))
+        image = cv2.rectangle(image, x1y1, x2y2, (255, 255, 255), 50)
     if predicted_labels is not None:
         for pred_label in predicted_labels:
             x1y1 = (pred_label[0]-pred_label[2]/2, pred_label[1]-pred_label[2]/2)
             x2y2 = (pred_label[0]+pred_label[2]/2, pred_label[1]+pred_label[2]/2)
             image = cv2.rectangle(image, x1y1, x2y2, (255, 0, 0), 2)
 
-    image = cv2.resize(image, dsize=(0, 0), fx=0.25, fy=0.25, interpolation=cv2.INTER_LINEAR)
-    cv2.imshow("sample", image)
-    cv2.waitKey(0)
+    image = cv2.resize(image, dsize=(0, 0), fx=0.1, fy=0.1, interpolation=cv2.INTER_LINEAR)
+    cv2.imwrite('sample.png', image)
+    # cv2.imshow("sample", image)
+    # cv2.waitKey(0)
 
 
-if __name__=="__main__":
-    draw_rectangles('../data/raw/raw_images_0.npy', '../data/raw/raw_labels_0.npy')
+def draw_cropped_images(target_dir='../data/cropped_samples',
+                        cropped_image_path='../data/cropped_resized_raw/cropped_resized_images_0.npy',
+                        cropped_label_path='../data/cropped_resized_raw/cropped_resized_labels_0.npy',
+                        predicted_labels=None):
+    images = np.load(cropped_image_path, allow_pickle=True)
+    label_list = np.load(cropped_label_path, allow_pickle=True)
+    image_number = cropped_image_path.split('_')[-1].split('.')[0]
+    for i_data, (image, labels) in enumerate(zip(images, label_list)):
+        images[i_data] = images[i_data]*255
+        for label in labels:
+            x1y1 = (int(label[0]-label[2]/2), int(label[1]-label[2]/2))
+            x2y2 = (int(label[0]+label[2]/2), int(label[1]+label[2]/2))
+            images[i_data] = cv2.rectangle(image, x1y1, x2y2, (255, 255, 255), 50)
+
+    if predicted_labels is not None:
+        for i_data, (image, pred_labels) in enumerate(zip(images, predicted_labels)):
+            for pred_label in pred_labels:
+                x1y1 = (pred_label[0]-pred_label[2]/2, pred_label[1]-pred_label[2]/2)
+                x2y2 = (pred_label[0]+pred_label[2]/2, pred_label[1]+pred_label[2]/2)
+                images[i_data] = cv2.rectangle(image, x1y1, x2y2, (255, 0, 0), 2)
+
+    for i_image, image in enumerate(images):
+        image = cv2.resize(image, dsize=(0, 0), fx=0.1, fy=0.1, interpolation=cv2.INTER_LINEAR)
+        image_path = os.path.join(target_dir, 'sample_{}_{}.png'.format(image_number, i_image))
+        cv2.imwrite(image_path, image)
+
+
+if __name__ == "__main__":
+    # draw_raw_image('../data/raw/raw_images_0.npy', '../data/raw/raw_labels_0.npy')
+    draw_cropped_images()
